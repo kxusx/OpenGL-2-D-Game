887747c1a7a4c145d013c45cd7298b1a9460e964
kxusx <khush99199@gmail.com>
2022-02-02 18:52:35 +0530

    1

---
+++ build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/khushpatel/Documents/Computer Graphics/CG-Assignment1/src/game.cpp" "/Users/khushpatel/Documents/Computer Graphics/CG-Assignment1/build/CMakeFiles/app.dir/src/game.cpp.o"
+  "/Users/khushpatel/Documents/Computer Graphics/CG-Assignment1/src/main.cpp" "/Users/khushpatel/Documents/Computer Graphics/CG-Assignment1/build/CMakeFiles/app.dir/src/main.cpp.o"
+  "/Users/khushpatel/Documents/Computer Graphics/CG-Assignment1/src/resource_manager.cpp" "/Users/khushpatel/Documents/Computer Graphics/CG-Assignment1/build/CMakeFiles/app.dir/src/resource_manager.cpp.o"
+  "/Users/khushpatel/Documents/Computer Graphics/CG-Assignment1/src/shader.cpp" "/Users/khushpatel/Documents/Computer Graphics/CG-Assignment1/build/CMakeFiles/app.dir/src/shader.cpp.o"
+  "/Users/khushpatel/Documents/Computer Graphics/CG-Assignment1/src/sprite_renderer.cpp" "/Users/khushpatel/Documents/Computer Graphics/CG-Assignment1/build/CMakeFiles/app.dir/src/sprite_renderer.cpp.o"
+  "/Users/khushpatel/Documents/Computer Graphics/CG-Assignment1/src/texture.cpp" "/Users/khushpatel/Documents/Computer Graphics/CG-Assignment1/build/CMakeFiles/app.dir/src/texture.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_INCLUDE_NONE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../libraries/glfw/include"
+  "../libraries/glad/include"
+  "../libraries/glm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/khushpatel/Documents/Computer Graphics/CG-Assignment1/build/libraries/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/Users/khushpatel/Documents/Computer Graphics/CG-Assignment1/build/CMakeFiles/glad.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
